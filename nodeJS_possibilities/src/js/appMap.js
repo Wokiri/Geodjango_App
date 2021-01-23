55c53519a74edd2eccc446ffc43d866f6299cc41
Joe Oketch <wokirijoe@gmail.com>
2021-01-23 11:48:29 +0300

    Labels to map features

--- nodeJS_possibilities/src/js/appMap.js
+++ nodeJS_possibilities/src/js/appMap.js
@@ -4,9 +4,14 @@ import GeoJSON from 'ol/format/GeoJSON';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import sync from 'ol-hashed';
-import { Style, Fill, Text, Stroke } from 'ol/style';
+import {
+  Circle as CircleStyle,
+  Style,
+  Fill,
+  Text,
+  Stroke
+} from 'ol/style';
 import { click } from 'ol/events/condition'
-
 
 const kenyaRegions = new VectorSource({
   features: new GeoJSON().readFeatures(kenyaRegionsJson, {
@@ -16,48 +21,143 @@ const kenyaRegions = new VectorSource({
   })
 })
 
+const kenyaOffices = new VectorSource({
+  features: new GeoJSON().readFeatures(kenyaOfficesJson, {
+      // dataProjection: 'EPSG:4326',
+      // featureProjection: 'EPSG:3857',
+      extractGeometryName: true
+  })
+})
+
 const map_container = document.querySelector('#map_container')
 
-const regionsTextLabel = feature => feature.get('Region')
+const regionsTextLabel = feature => `${feature.get('region')} Region`
+const officesTextLabel = feature => `${feature.get('name')} Office`
 
 const regionsTextStyle = feature => (
     new Text({
         textAlign: 'center',
         textBaseline: 'middle',
-        font: "14px verdana, Calibri, sans-serif",
+        font: `bold 14px "Trebuchet MS", Helvetica, sans-serif`,
         text: regionsTextLabel(feature),
         placement: 'polygon',
         fill: new Fill({
-            color: 'rgb(256, 256, 256)'
+          color: 'rgb(40, 40, 164)'
         })
     })
 )
+const officesTextStyle = feature => {
+  if (officesTextLabel(feature) === 'Nairobi Office'){
+    return new Text({
+      textAlign: 'center',
+      textBaseline: 'middle',
+      font: `bold 16px "Trebuchet MS", Helvetica, sans-serif`,
+      text: (officesTextLabel(feature)).toUpperCase(),
+      placement: 'point',
+      offsetX: 0,
+      offsetY:20,
+      fill: new Fill({
+        color: 'rgb(5, 80, 10)'
+      })
+    })
+  }else if (officesTextLabel(feature) === 'Kisumu Office' || officesTextLabel(feature) === 'Thika Office' || officesTextLabel(feature) === 'Mombasa Office'){
+    return new Text({
+      textAlign: 'start',
+      textBaseline: 'middle',
+      font: `bold 14px "Trebuchet MS", Helvetica, sans-serif`,
+      text: officesTextLabel(feature),
+      placement: 'point',
+      offsetX: 12,
+      offsetY:0,
+      fill: new Fill({
+        color: 'rgb(13, 13, 38)'
+      })
+    })
+
+  } else{
+    return new Text({
+      textAlign: 'start',
+      textBaseline: 'middle',
+      font: `bold 12px "Trebuchet MS", Helvetica, sans-serif`,
+      text: officesTextLabel(feature),
+      placement: 'point',
+      offsetX: 10,
+      offsetY: 0,
+      fill: new Fill({
+        color: 'rgb(255, 255, 255)'
+      })
+    })
+  }
+    
+}
 
 const regionsPolygonStyle = feature =>  {
   return new Style({
-    fill: new Fill({
-      color: 'rgba(214, 214, 245, 1)'
-    }),
-    stroke: new Stroke({
-      color: 'rgb(20, 20, 82)',
-      width: 1
-    }),
-    text: regionsTextStyle(feature)
-  })
+      fill: new Fill({
+        color: 'rgba(140, 140, 217, 1)'
+      }),
+      stroke: new Stroke({
+        color: 'rgb(20, 20, 82)',
+        width: 1
+      }),
+      text: regionsTextStyle(feature)
+    })
 }
 
-// timezones layer
+
+// Ofices style
+const officesPointStyle = feature =>  {
+
+  if (officesTextLabel(feature) === 'Nairobi Office'){
+    return new Style({
+    image: new CircleStyle({
+      radius: 10,
+      fill: new Fill({color: 'rgba(40, 80, 80, 0.1)'}),
+      stroke: new Stroke({color: 'rgb(40,80, 80)', width: 4}),
+    }),
+    text: officesTextStyle(feature)
+  })
+
+  }else if(officesTextLabel(feature) === 'Kisumu Office' || officesTextLabel(feature) === 'Thika Office' || officesTextLabel(feature) === 'Mombasa Office'){
+    return new Style({
+    image: new CircleStyle({
+      radius: 7,
+      fill: new Fill({color: 'rgba(255, 255, 255, 1)'}),
+      // stroke: new Stroke({color: 'rgb(80, 120, 120)', width: 3}),
+    }),
+    text: officesTextStyle(feature)
+  })
+
+  }else{
+    return new Style({
+    image: new CircleStyle({
+      radius: 5,
+      fill: new Fill({color: 'rgba(40, 80, 80, 1)'}),
+      stroke: new Stroke({color: 'rgb(200, 240, 240)', width: 2}),
+    }),
+    text: officesTextStyle(feature)
+  })
+  }
+}
+
+// regions layer
 const kenyaRegionsLayer = new VectorLayer({
     source: kenyaRegions,
     style: regionsPolygonStyle
 })
 
+// offices layer
+const kenyaOfficesLayer = new VectorLayer({
+    source: kenyaOffices,
+    style: officesPointStyle
+})
+
 const geodjangoMap = new Map({
   target: map_container,
-  layers: [kenyaRegionsLayer],
+  layers: [kenyaRegionsLayer, kenyaOfficesLayer],
   view: new View({
     center: [36.8166667, -1.2833333],
-    zoom: 24
+    zoom: 25
   })
 });
 
