c5f0f42bf97394875aaddb0d6f0d74e0d6dfddf3
Joe Oketch <wokirijoe@gmail.com>
2021-01-23 18:10:30 +0300

    Selection 1

--- nodeJS_possibilities/src/js/appMap.js
+++ nodeJS_possibilities/src/js/appMap.js
@@ -11,7 +11,10 @@ import {
   Text,
   Stroke
 } from 'ol/style';
-import { click } from 'ol/events/condition'
+import { click, singleClick } from 'ol/events/condition'
+import Select from 'ol/interaction/Select'
+import Overlay from 'ol/Overlay';
+
 
 const kenyaRegions = new VectorSource({
   features: new GeoJSON().readFeatures(kenyaRegionsJson, {
@@ -152,13 +155,164 @@ const kenyaOfficesLayer = new VectorLayer({
     style: officesPointStyle
 })
 
+
+const theOverlay = new Overlay({
+  element: container,
+  autoPan: true,
+  autoPanAnimation: {
+	  duration: 250
+  }
+});
+
 const geodjangoMap = new Map({
   target: map_container,
   layers: [kenyaRegionsLayer, kenyaOfficesLayer],
+  overlays: [theOverlay],
   view: new View({
     center: [36.8166667, -1.2833333],
     zoom: 25
   })
-});
+})
+
+
+
+const mapAndContents = document.getElementById('map_and_contents')
+
+let container
+let closer
+let content
+
+geodjangoMap.on('singleclick', () => {
+	
+	// Elements that will form a pop-up
+  container = document.createElement("DIV")
+  container.setAttribute("class", "ol-popup");
+  container.setAttribute("id", "popup");
+  mapAndContents.appendChild(container)
+	
+	closer = document.createElement("A")
+  closer.setAttribute("class", "ol-popup-closer");
+  closer.setAttribute("id", "popup-closer");
+  container.appendChild(closer)
+	
+	content = document.createElement("DIV")
+  content.setAttribute("id", "popup-content");
+  container.appendChild(content)
+})
+
+const btnRegionCloser = document.getElementById('btn-region-closer')
+const btnOfficeCloser = document.getElementById('btn-office-closer')
+
+
+// Regions selection options
+const regionSingleClick = new Select({
+  layers: [kenyaRegionsLayer]
+}) //By default, this is module:ol/events/condition~singleClick. Other defaults (except layers) are exactly what I need
+
+// If region is selected get feature info, don't otherwise
+const bringRegionPopupInfo = theFeature => {
+	let regionFeatureName = theFeature.get('region')
+	
+	geodjangoMap.on('singleclick', evt => {
+	let coordinates = evt.coordinate
+	content.innerHTML = `
+	<h3 class="text-center m-0">${regionFeatureName}</h3>
+	<ul class="nav justify-content-center m-0">
+		<li class="nav-item">
+			<button id="btn-region-closer" type="button" class="btn btn-outline-primary p-0"><a class="nav-link" href="#">Region Information</a></button>
+		</li>
+	</ul>
+	`
+	theOverlay.setPosition(coordinates);
+})
+}
+
+geodjangoMap.addInteraction(regionSingleClick)
+regionSingleClick.on('select', elem => {
+	// While at it, get its attributes
+	bringRegionPopupInfo(elem.target)
+})
+
+
+// Offices selection options
+const officesSingleClick = new Select({
+  layers: [kenyaOfficesLayer]
+}) //By default, this is module:ol/events/condition~singleClick. Other defaults (except layers) are exactly what I need
+
+// If office is selected get feature info, don't otherwise
+const bringOfficePopupInfo = theFeature => {
+	let officeFeatureName = theFeature.get('name')
+	
+	geodjangoMap.on('singleclick', evt => {
+		let coordinates = evt.coordinate
+		content.innerHTML = `
+		<h3 class="text-center m-0">${officeFeatureName}</h3>
+		<ul class="nav justify-content-center m-0">
+			<li class="nav-item">
+				<button id="btn-office-closer" type="button" class="btn btn-outline-primary p-0"><a class="nav-link" href="#">Office Information</a></button>
+			</li>
+		</ul>
+		`
+		theOverlay.setPosition(coordinates);
+	})
+}
+
+geodjangoMap.addInteraction(officesSingleClick)
+officesSingleClick.on('select', elem => {
+	// While at it, get its attributes
+	bringOfficePopupInfo(elem.target)
+})
+
+if (btnOfficeCloser) {
+  btnOfficeCloser.addEventListener('click', () => {
+    theOverlay.setPosition(undefined)
+    btnOfficeCloser.blur()
+    return false
+  })
+}
+
+if (btnRegionCloser){
+    btnRegionCloser.addEventListener('click', () => {
+    theOverlay.setPosition(undefined)
+    btnRegionCloser.blur()
+    return false
+  })
+}
+
+if (closer){
+  closer.addEventListener('click', () => {
+  theOverlay.setPosition(undefined)
+  closer.blur()
+  return false
+  })
+}
 
 sync(geodjangoMap);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
